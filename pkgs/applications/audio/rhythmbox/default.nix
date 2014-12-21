0e572b1cee1fd531f911868689ecff83d64045c1
Albin Stjerna <albin.stjerna@gmail.com>
2014-12-21 12:00:58 +0100

    Add rhythmbox.

--- pkgs/applications/audio/rhythmbox/default.nix
+++ pkgs/applications/audio/rhythmbox/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, gobjectIntrospection, gnome3}:
+{ stdenv, fetchurl, gobjectIntrospection, gnome3, gtk3, glib, intltool, libtool, pkgconfig, tdb, json_glib, gst_all_1, libnotify, libmtp, clutter, itstool, python3,hicolor_icon_theme, shared_mime_info}:
 
 stdenv.mkDerivation rec {
   name = "rhythmbox-3.1";
@@ -8,11 +8,26 @@ stdenv.mkDerivation rec {
     sha256 = "2dc4f80080943ba8d2450cc4f9c4978f741efa26a6b1599ef68f57f7b2d7fcc1";
   };
 
-  buildInputs = [gobjectIntrospection ];
+propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard
+                      gnome3.gnome_icon_theme hicolor_icon_theme
+                      shared_mime_info
+                      gnome3.gnome_icon_theme_symbolic ];
+
+  buildInputs = [gobjectIntrospection intltool glib gtk3 libtool pkgconfig gnome3.libsoup gnome3.libpeas tdb json_glib gnome3.totem-pl-parser gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good gst_all_1.gst-plugins-ugly libnotify libmtp clutter itstool python3 gnome3.gsettings_desktop_schemas];
 
 #  configurePhase = ''
 #  ./autogen.sh --prefix=$out
 #  '';
+
+# From the Empathy package
+preFixup = ''
+for f in $out/bin/* $out/libexec/*; do
+wrapProgram $f \
+--set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
+--prefix XDG_DATA_DIRS : "$XDG_ICON_DIRS:${gtk3}/share:${gnome3.gnome_themes_standard}/:${gnome3.gnome_themes_standard}/share:${hicolor_icon_theme}/share:$out/share:$GSETTINGS_SCHEMAS_PATH"
+done
+rm $out/share/icons/hicolor/icon-theme.cache
+'';
 
   meta = {
     homepage = https://wiki.gnome.org/Apps/Rhythmbox;
